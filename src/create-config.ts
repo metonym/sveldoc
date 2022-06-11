a7a441e29f3e407e8d9abcf5488195b2e82bcfd2
Eric Liu <ericyl.us@gmail.com>
2022-06-11 12:17:32 -0700

    fix(create-config): avoid "invalid plugin options" warning

--- src/create-config.ts
+++ src/create-config.ts
@@ -103,7 +103,6 @@ export const createConfig: (config: CreateConfigOptions) => Promise<SvelteKitCon
         ...config?.mdsvexOptions,
       }),
     ],
-    ...config,
     kit: {
       adapter: config?.adapter,
       files: config?.files,
