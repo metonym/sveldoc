d1cac8f22037102288b74b03795467075f0ba3e0
Eric Liu <ericyl.us@gmail.com>
2022-09-03 11:55:26 -0700

    refactor: extract index matcher

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,4 +1,5 @@
 export const match: Record<string, (str: string) => boolean> = {
+  indexFile: (filename) => /index.html$/.test(filename),
   readmeFile: (filename) => /readme.md$/i.test(filename),
   exampleStart: (line) => /^<!-- example-start/.test(line),
   exampleEnd: (line) => /^<!-- example-end -->/.test(line),
